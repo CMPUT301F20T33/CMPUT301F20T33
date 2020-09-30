9c96ce3c3cf1dfc01ad6779d6648e0266b2aa35b
Chloe Ching <cching@ualberta.ca>
2020-09-30 16:53:17 -0600

    deleted extra

--- app/src/main/java/com/example/cmput301f20t33/Shape.java
+++ app/src/main/java/com/example/cmput301f20t33/Shape.java
@@ -3,11 +3,7 @@ package com.example.cmput301f20t33;
 public abstract class Shape {
     int x;
     int y;
-<<<<<<< HEAD
 
-    
-
-=======
     String color = "Black";
->>>>>>> 6c57bccad94229655175deb650295330548e0316
+
 }
