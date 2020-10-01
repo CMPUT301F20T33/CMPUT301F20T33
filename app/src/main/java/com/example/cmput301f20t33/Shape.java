3779cb603ce463f4359dd59f15e36e802dd57524
Kimberly Tran <ktran5@ualberta.ca>
2020-10-01 15:03:38 -0600

    Resolved conflict

--- app/src/main/java/com/example/cmput301f20t33/Shape.java
+++ app/src/main/java/com/example/cmput301f20t33/Shape.java
@@ -5,6 +5,6 @@ package com.example.cmput301f20t33;
 public abstract class Shape {
     int x;
     int y;
-    String color = blue;
+    String color = "blue";
 }
 >>>>>>> e9a4a0cba40154a32b35fb59184e140aa2332f79
